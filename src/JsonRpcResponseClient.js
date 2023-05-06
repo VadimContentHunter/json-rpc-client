e2f6389421cb3df8d49fedd860d4bd15e1a29fb4
Vadim <project.k.vadim@gmail.com>
2023-05-06 15:47:03 +0300

    created JsonRpcResponseClient class

---
+++ src/JsonRpcResponseClient.js
@@ -0,0 +1,142 @@
+import JsonRpcRequestError from '../src/errors/JsonRpcRequestError.js';
+import { JsonRpcError } from '../src/JsonRpcError.js';
+
+export class JsonRpcResponseClient {
+    #lockVersion = '2.0';
+    #version;
+    #result;
+    #error;
+    #id;
+
+    constructor(json) {
+        this.initializeObject(json);
+    }
+
+    get version() {
+        if (typeof this.#version !== 'string') {
+            throw new JsonRpcRequestError('Поле version не является строкой.');
+        }
+
+        if (
+            this.#version === '' ||
+            this.#version.match(/^(\s+).*$/i) !== null ||
+            this.#version.match(/.*(\s)$/i) !== null
+        ) {
+            throw new JsonRpcRequestError(
+                'Поле version не должно быть пустой строкой или содержать пробелы в начале и конце.',
+            );
+        }
+
+        return this.#version;
+    }
+
+    get id() {
+        if (typeof this.#id !== 'number' && this.#id !== null) {
+            throw new JsonRpcRequestError('Поле id не является числом или null.');
+        }
+
+        if (this.#id < 0 && this.#id !== null) {
+            throw new JsonRpcRequestError('Поле id не должно быть меньше 0.');
+        }
+
+        return this.#id;
+    }
+
+    get result() {
+        if (typeof this.#result === 'undefined') {
+            throw new JsonRpcRequestError('Поле result должно иметь значение.');
+        }
+
+        if (typeof this.#result !== 'undefined' && this.#error instanceof JsonRpcError) {
+            throw new JsonRpcRequestError(
+                'Только одно поле должно иметь значение ИЛИ JsonRpcResponseClient.result ИЛИ JsonRpcResponseClient.error!',
+            );
+        }
+
+        return this.#result;
+    }
+
+    get error() {
+        if (this.#error instanceof JsonRpcError) {
+            throw new JsonRpcRequestError('Поле error должно быть объектом JsonRpcError класса.');
+        }
+
+        if (typeof this.#result !== 'undefined' && this.#error instanceof JsonRpcError) {
+            throw new JsonRpcRequestError(
+                'Только одно поле должно иметь значение ИЛИ JsonRpcResponseClient.result ИЛИ JsonRpcResponseClient.error!',
+            );
+        }
+
+        return this.#error;
+    }
+
+    initializeObject(json) {
+        if (typeof json !== 'string') {
+            throw new JsonRpcRequestError('Параметр json не является строкой.');
+        }
+
+        let object = JSON.parse(json, (key, value) => {
+            if (key === 'error') return new JsonRpcError(value);
+            return value;
+        });
+
+        if (!Object.hasOwn(object, 'version') && !Object.hasOwn(object, 'jsonrpc')) {
+            throw new JsonRpcRequestError('Не удалось найти в json свойство version или jsonrpc');
+        }
+
+        if (Object.hasOwn(object, 'jsonrpc')) {
+            object.version = object.jsonrpc;
+        }
+
+        if (!Object.hasOwn(object, 'id')) {
+            throw new JsonRpcRequestError('Не удалось найти в json свойство id');
+        }
+
+        if (!Object.hasOwn(object, 'result') && !Object.hasOwn(object, 'error')) {
+            throw new JsonRpcRequestError('Не удалось найти в json свойство result или error');
+        }
+
+        if (typeof object.version !== 'string') {
+            throw new JsonRpcRequestError('Поле object.version не является строкой.');
+        }
+
+        if (
+            object.version === '' ||
+            object.version.match(/^(\s+).*$/i) !== null ||
+            object.version.match(/.*(\s)$/i) !== null
+        ) {
+            throw new JsonRpcRequestError(
+                'Поле object.version не должно быть пустой строкой или содержать пробелы в начале и конце.',
+            );
+        }
+
+        if (object.version !== this.#lockVersion) {
+            throw new JsonRpcRequestError('Версия ответа не ' + this.#lockVersion);
+        }
+
+        if (Number.isInteger(object.id) && object.id !== null) {
+            throw new JsonRpcRequestError('Поле object.id не является числом или null.');
+        }
+
+        if (object.id < 0 && object.id !== null) {
+            throw new JsonRpcRequestError('Поле object.id не должно быть меньше 0.');
+        }
+
+        if (typeof object.result === 'undefined' && !(object.error instanceof JsonRpcError)) {
+            throw new JsonRpcRequestError('Одно из полей object.result или object.error должно иметь значение.');
+        }
+
+        if (typeof object.result !== 'undefined' && object.error instanceof JsonRpcError) {
+            throw new JsonRpcRequestError('Только одно поле должно иметь значение ИЛИ object.result ИЛИ object.error!');
+        }
+
+        this.#version = object.version;
+        this.#id = Number(object.id);
+
+        if (typeof object.result === 'undefined') {
+            this.#error = object.error;
+        } else {
+            this.#result = object.result;
+        }
+    }
+}
